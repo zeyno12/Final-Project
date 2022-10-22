0b1a02773006a5f4642fa4bf6207748d6ac5a124
zeyno12 <zeynabbagirova1@gmail.com>
2022-10-23 01:49:27 +0400

    creating

---
+++ assets/Mock/infolab.js
@@ -0,0 +1,119 @@
+let data = [
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+  {
+    image:
+      "https://www.saglamaile.az/site/assets/files/12783/menopauza.283x171.png",
+    name: "Menopauza",
+    content:
+      " Klimaks və ya menopauza – insan həyatındafiziolojı dövrdür. Qadında menstruasiyanın (aybaşı) ardıcıl...",
+    month: "18 Oktyabr 2022",
+    view: "Baxış sayı: 37",
+  },
+];
--- assets/js/app.js
+++ assets/js/app.js
@@ -23,11 +23,86 @@ function openitem(e) {
   e.preventDefault();
   transformitem.classList.toggle("active");
 }
-
+// /accordion
 let accordion = document.getElementsByClassName("accordion");
 let array = [...accordion];
 array.forEach((element) => {
   element.addEventListener("click", function () {
-    element.nextElementSibling.classList.toggle("panel_row");
+    element.nextElementSibling.classList.toggle("active2");
   });
 });
+
+//tab_items
+const labels = document.querySelectorAll(".accordion-item__label");
+const tabs = document.querySelectorAll(".accordion-tab");
+
+function toggleShow() {
+  const target = this;
+  const item = target.classList.contains("accordion-tab")
+    ? target
+    : target.parentElement;
+  const group = item.dataset.actabGroup;
+  const id = item.dataset.actabId;
+
+  tabs.forEach(function (tab) {
+    if (tab.dataset.actabGroup === group) {
+      if (tab.dataset.actabId === id) {
+        tab.classList.add("accordion-active");
+      } else {
+        tab.classList.remove("accordion-active");
+      }
+    }
+  });
+
+  labels.forEach(function (label) {
+    const tabItem = label.parentElement;
+
+    if (tabItem.dataset.actabGroup === group) {
+      if (tabItem.dataset.actabId === id) {
+        tabItem.classList.add("accordion-active");
+      } else {
+        tabItem.classList.remove("accordion-active");
+      }
+    }
+  });
+}
+
+labels.forEach(function (label) {
+  label.addEventListener("click", toggleShow);
+});
+
+tabs.forEach(function (tab) {
+  tab.addEventListener("click", toggleShow);
+});
+
+///////
+fetch("/assets/Mock/infolab.js")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    let output = "";
+    data.forEach((data) => {
+      output += `
+    <div class="lab_card">
+    <div class="lab_image">
+      <img src=${data.image} alt=${data.name} />
+    </div>
+    <div class="lab_items">
+      <h4>${data.name}</h4>
+      <p>${data.content}</p>
+      <div class="lab_data">
+        <div class="month">
+          <p>${data.month}</p>
+        </div>
+        <div class="view">
+          <p>${data.view}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    });
+
+    document.querySelector(".col-lg-4").innerHTML = output;
+  })
+  .catch((err) => console.log(err));
