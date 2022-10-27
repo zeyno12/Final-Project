07a4762883a65c71a261c1b60b8518042b892306
zeyno12 <zeynabbagirova1@gmail.com>
2022-10-28 02:30:02 +0400

    comig soon finish look

--- assets/js/main_search.js
+++ assets/js/main_search.js
@@ -5,31 +5,24 @@ function item(e) {
   e.preventDefault();
   openSearch.classList.toggle("block");
 }
-
-document.getElementById("searchAll").addEventListener("", filterList);
-function filterList() {
-  const searchInput = document.getElementById("search");
-  const filter = searchInput.value.toLowerCase();
-  const listItems = document.querySelectorAll(".axtaris_items");
-  listItems.forEach((item) => {
-    let text = item.textContent;
-    if (text.toLowerCase().includes(filter.toLowerCase())) {
-      item.style.display = "";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
-let form1 = document.getElementById("form1");
-form1.onclick = function (e) {
-  e.preventDefault();
-};
-let searchCl = document.getElementsByClassName("main_serch");
-searchCl.addEventListener("keydown", function () {
-  alert("Privet");
-});
-searchCl.addEventListener("keydown", (event) => {
-  if (event.isComposing || event.keyCode === 13) {
-    alert("Privet");
+var input = document.getElementById("searchAll");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    location.href = "http://127.0.0.1:5500/axtariw.html";
   }
 });
+// document.getElementById("search").addEventListener("input", filterList);
+// function filterList() {
+//   const searchInput = document.getElementById("search");
+//   const filter = searchInput.value.toLowerCase();
+//   const listItems = document.querySelectorAll("axtaris_items");
+//   listItems.forEach((item) => {
+//     let text = item.textContent;
+//     if (text.toLowerCase().includes(filter.toLowerCase())) {
+//       item.style.display = "";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+// }
