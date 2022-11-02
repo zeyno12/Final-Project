469479eb577c0be478ec899e45687c1c1ec4ab14
zeyno12 <zeynabbagirova1@gmail.com>
2022-11-02 12:16:50 +0400

    change color pages

--- assets/js/nav_item.js
+++ assets/js/nav_item.js
@@ -4,5 +4,6 @@ const transformitem = document.querySelector(".transform_itemas");
 opendata.addEventListener("click", openitem);
 function openitem(e) {
   e.preventDefault();
+
   transformitem.classList.toggle("active");
 }
